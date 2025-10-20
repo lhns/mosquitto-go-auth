b35d68f5cd0cbfb57dfeadd180b4f2bf893de96e
Pierre Kisters <1524059+lhns@users.noreply.github.com>
2025-10-21 01:40:48 +0200

    fix binary builds failing silently and fix arm64 build by pinning the golang image (#39)
    * Add error handling and set options in build script
    
    * run release-build.yml on commit
    
    Added push trigger for all branches to the release build workflow.
    
    * pin golang image and print current architecture in linux binary build

--- .github/workflows/scripts/build.sh
+++ .github/workflows/scripts/build.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+set -eo pipefail
+
 apt-get update
 apt-get install -y gcc-arm-linux-gnueabi binutils-arm-linux-gnueabi gcc-aarch64-linux-gnu binutils-aarch64-linux-gnu
 cd /usr/src/mosquitto-$MOSQUITTO_VERSION/include
 cp *.h /usr/include
 cd /usr/src/mosquitto-go-auth
 
-#build amd64 Linux
+echo "build amd64 Linux" >&2
 make without-vcs
 cp go-auth.so pw /usr/src/output/linux-amd64
 
-# build arm64 Linux
+echo "build arm64 Linux" >&2
 make clean
 export CGO_ENABLED=1
 export GOARCH=arm64
@@ -18,7 +20,7 @@ export CC=aarch64-linux-gnu-gcc
 make without-vcs
 cp go-auth.so pw /usr/src/output/linux-arm64
 
-# build armv7 Linux
+echo "build armv7 Linux" >&2
 make clean
 export CGO_ENABLED=1
 export GOARCH=arm
@@ -27,7 +29,7 @@ export CC=arm-linux-gnueabi-gcc
 make without-vcs
 cp go-auth.so pw /usr/src/output/linux-armv7
 
-# build armv7 Linux
+echo "build armv7 Linux" >&2
 make clean
 export CGO_ENABLED=1
 export GOARCH=arm
